3fdb30253f179abe1dac1535e4f6acf304d99669
changnet <940477049@qq.com>
2019-09-23 22:42:53 +0800

    lsp server

--- src/client/extension.ts
+++ src/client/extension.ts
@@ -2,25 +2,77 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import * as path from 'path';
+
+// add "vscode-languageclient": "^5.2.1" in package.json and run: npm install
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
+} from 'vscode-languageclient';
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "lua-tags" is now active!');
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "lua-tags" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+    // https://code.visualstudio.com/api/language-extensions/language-server-extension-guide
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+    // 指定服务器入口文件路径
+    let serverModule = context.asAbsolutePath(path.join('out', 'server', 'server.js'));
 
-	context.subscriptions.push(disposable);
+    // debug选项
+    // 必须用F5调试模式打开插件，才有效
+    // --inspect=6009表示server.js在6009监听debug连接
+    // 在Debug-->Add Configuration里加一个选项Attack，注意新增的端口和监听端口对应
+    // 用F5打开插件后，创建对应的文件激活插件，然后在左边栏切换到Debug View
+    // 在Debug选项里选择Attach，即可连接到服务器，这时就可以断点调试server了
+    let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
+    console.log(`server path:${serverModule}`)
+
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    let serverOptions: ServerOptions = {
+        run: { module: serverModule, transport: TransportKind.ipc },
+        debug: {
+            module: serverModule,
+            transport: TransportKind.ipc,
+            options: debugOptions
+        }
+    };
+
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+        // Register the server for lua documents
+        documentSelector: [{ scheme: 'file', language: 'lua' }],
+        synchronize: {
+            // Notify the server about file changes
+            configurationSection: ['lua']
+        }
+    };
+
+    // Create the language client and start the client.
+    let client = new LanguageClient(
+        'languageServerLua',
+        'Language Server Lua',
+        serverOptions,
+        clientOptions
+    );
+
+    // "languageServerLua.trace.server": "verbose"
+    // 把这个添加到设置(没错，就是File-->Preferences-->Setting)
+    // F5调试插件，在新打开的的vsc里的控制台OUTPUT里即可看到这个选项，可以看到server日志
+
+    // Start the client. This will also launch the server
+    const disposable = client.start();
+
+    // 在插件deactivate时，把这个client销毁掉
+    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
---
+++ src/server/server.ts
@@ -0,0 +1,88 @@
+import {
+    createConnection,
+    TextDocuments,
+    TextDocument,
+    Diagnostic,
+    DiagnosticSeverity,
+    ProposedFeatures,
+    InitializeParams,
+    DidChangeConfigurationNotification,
+    CompletionItem,
+    CompletionItemKind,
+    TextDocumentPositionParams
+  } from 'vscode-languageserver';
+
+// https://code.visualstudio.com/api/language-extensions/language-server-extension-guide
+class Server
+{
+    // Create a connection for the server. The connection uses Node's IPC as a transport.
+    // Also include all preview / proposed LSP features.
+    private connection = createConnection(ProposedFeatures.all);
+
+    // Create a simple text document manager. The text document manager
+    // supports full document sync only
+    private documents: TextDocuments = new TextDocuments();
+
+    private rootUri: string | null = null;
+
+    // 哪些字符触发函数提示
+    private readonly triggerCharacters = ['.', ':'];
+
+    public constructor()
+    {
+        this.connection.onInitialize(handler => this.onInitialize(handler));
+        this.connection.onInitialized(() => this.onInitialized());
+        this.connection.onCompletion(pos => this.onCompletion(pos));
+    }
+
+    public init()
+    {
+        this.documents.listen(this.connection);
+        this.connection.listen();
+    }
+
+    private onInitialize(initializeParams: InitializeParams) {
+        this.rootUri = initializeParams.rootUri;
+
+        return {
+            capabilities: {
+                // Use full sync mode for now.
+                // TODO: Add support for Incremental changes. Full syncs will not scale very well.
+                textDocumentSync: this.documents.syncKind,
+                //documentSymbolProvider: true, // 单个文档符号 CTRL + SHIFT + O
+                //workspaceSymbolProvider: true, // 整个工程符号 CTRL + T
+                completionProvider: { // 打.或者:时列出可自动完成的函数
+                    resolveProvider: true,
+                    triggerCharacters: this.triggerCharacters
+                }
+                //documentFormattingProvider: true, // 格式化整个文档
+                //documentRangeFormattingProvider: true // 格式化选中部分
+            }
+        };
+    }
+
+    private onInitialized()
+    {
+        this.connection.console.log("Lua LSP Server started")
+    }
+
+    private onCompletion(pos: TextDocumentPositionParams): CompletionItem[]
+    {
+        return [
+            {
+              label: 'TypeScript',
+              kind: CompletionItemKind.Text,
+              data: 1
+            },
+            {
+              label: 'JavaScript',
+              kind: CompletionItemKind.Text,
+              data: 2
+            }
+          ];
+    }
+}
+
+let srv = new Server()
+srv.init()
+
