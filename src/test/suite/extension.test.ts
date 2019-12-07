2a5f0132ee3087df062ddaae39c06318becc3f77
changnet <940477049@qq.com>
2019-12-07 17:57:19 +0800

    prepare for test

---
+++ src/test/suite/extension.test.ts
@@ -0,0 +1,21 @@
+import * as assert from 'assert';
+import { before } from 'mocha';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+// import * as myExtension from '../extension';
+
+// A GOOD example from Microsoft
+// https://github.com/microsoft/vscode-go/blob/master/test/integration/extension.test.ts
+
+suite('Extension Test Suite', () => {
+	before(() => {
+		vscode.window.showInformationMessage('Start all tests.');
+	});
+
+	test('Sample test', () => {
+		assert.equal(-1, [1, 2, 3].indexOf(5));
+		assert.equal(-1, [1, 2, 3].indexOf(0));
+	});
+});
