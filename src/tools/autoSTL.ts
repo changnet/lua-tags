7f499ca39e10a765da234640784ce10577b56081
changnet <940477049@qq.com>
2026-07-09 19:37:05 +0800

    add lua54 lua55 stl

--- src/tools/autoSTL.ts
+++ src/tools/autoSTL.ts
@@ -1,41 +1,55 @@
 // 自动导出lua stand library
 
-import * as fs from "fs";
-import { assert } from "console";
+import * as fs from 'fs';
+import { assert } from 'console';
+
+// 1. 下载对应版本的源码包解压，把里面的doc放到stl目录
+// 2. 配置好下面的版本参数
+// 3. 执行npm run stl
 
 // lua5.1.5的源码包中的contents.html不包含coroutine等模块的模块声明
 // 但是 http://www.lua.org/manual/5.1/ 中的是包含的，目前解析的是从浏览器下载而来的
 const stl = [
     {
         // jit using 5.1 manual now
-        v: "LuaJIT",
-        j: "stl/stl_LuaJIT.json",
-        c: "stl/doc_5_1/contents.html",
-        m: "stl/doc_5_1/manual.html",
+        v: 'LuaJIT',
+        j: 'stl/stl_LuaJIT.json',
+        c: 'stl/doc_5_1/contents.html',
+        m: 'stl/doc_5_1/manual.html',
     },
     {
-        v: "5.1",
-        j: "stl/stl_5.1.json",
-        c: "stl/doc_5_1/contents.html",
-        m: "stl/doc_5_1/manual.html",
+        v: '5.1',
+        j: 'stl/stl_5.1.json',
+        c: 'stl/doc_5_1/contents.html',
+        m: 'stl/doc_5_1/manual.html',
     },
     {
-        v: "5.2",
-        j: "stl/stl_5.2.json",
-        c: "stl/doc_5_2/contents.html",
-        m: "stl/doc_5_2/manual.html",
+        v: '5.2',
+        j: 'stl/stl_5.2.json',
+        c: 'stl/doc_5_2/contents.html',
+        m: 'stl/doc_5_2/manual.html',
     },
     {
-        v: "5.3",
-        j: "stl/stl_5.3.json",
-        c: "stl/doc_5_3/contents.html",
-        m: "stl/doc_5_3/manual.html",
-    }
+        v: '5.3',
+        j: 'stl/stl_5.3.json',
+        c: 'stl/doc_5_3/contents.html',
+        m: 'stl/doc_5_3/manual.html',
+    },
+    {
+        v: '5.4',
+        j: 'stl/stl_5.4.json',
+        c: 'stl/doc_5_4/contents.html',
+        m: 'stl/doc_5_4/manual.html',
+    },
+    {
+        v: '5.5',
+        j: 'stl/stl_5.5.json',
+        c: 'stl/doc_5_5/contents.html',
+        m: 'stl/doc_5_5/manual.html',
+    },
 ];
 
-import {
-    SymbolKind,
-} from 'vscode-languageserver';
+import { SymbolKind } from 'vscode-languageserver';
 
 // 记录单个符号的信息
 // TODO: 直接import symbol.ts中的SymInfoEx来处理？可能会有加载失败风险，比如版本不一致
@@ -67,15 +81,16 @@ function searchSymbol(ctx: string) {
 
     const symbols: SymbolEx[] = [];
     const lines = ctx.split(/\r?\n/g);
-    lines.forEach(line => {
+    lines.forEach((line) => {
         if (!line.startsWith('<A HREF="manual.html#')) {
             return;
         }
 
         // 模块 <A HREF="manual.html#6.6">table</A><BR>
         let matchs = line.match(
-            /^<A HREF="manual.html#([.0-9]+)">(.+?)<\/A><BR>$/);
-        if (matchs && "basic" !== matchs[2]) {
+            /^<A HREF="manual.html#([.0-9]+)">(.+?)<\/A><BR>$/,
+        );
+        if (matchs && 'basic' !== matchs[2]) {
             symbols.push({
                 name: matchs[2],
                 url: matchs[1],
@@ -85,9 +100,8 @@ function searchSymbol(ctx: string) {
         }
 
         // 函数 <A HREF="manual.html#pdf-table.concat">table.concat</A><BR>
-        matchs = line.match(
-            /^<A HREF="manual.html#pdf-(.+?)">(.+?)<\/A><BR>$/);
-        if (matchs && "basic" !== matchs[2]) {
+        matchs = line.match(/^<A HREF="manual.html#pdf-(.+?)">(.+?)<\/A><BR>$/);
+        if (matchs && 'basic' !== matchs[2]) {
             symbols.push({
                 name: matchs[2],
                 url: matchs[1],
@@ -116,9 +130,9 @@ Returns the absolute value of <code>x</code>. (integer/float)
  * @param from 从哪个位置开始解析
  */
 function searchDesc(ctx: string, from: number) {
-    const flag = "<p>\n";
+    const flag = '<p>\n';
     let begPos = from;
-    if (ctx.charAt(begPos) === "\n") {
+    if (ctx.charAt(begPos) === '\n') {
         begPos = ctx.indexOf(flag, from) + flag.length;
     }
 
@@ -126,7 +140,7 @@ function searchDesc(ctx: string, from: number) {
     let emptyLine = 0;
     let endPos = begPos;
     while (--maxLoop > 0) {
-        const linePos = ctx.indexOf("\n", endPos);
+        const linePos = ctx.indexOf('\n', endPos);
         if (endPos === linePos) {
             emptyLine++;
             if (emptyLine >= 2) {
@@ -138,7 +152,7 @@ function searchDesc(ctx: string, from: number) {
             continue;
         }
         const lineCtx = ctx.substring(endPos, linePos);
-        if (lineCtx.startsWith("<hr>") || lineCtx.startsWith("<ul>")) {
+        if (lineCtx.startsWith('<hr>') || lineCtx.startsWith('<ul>')) {
             break;
         }
 
@@ -151,20 +165,20 @@ function searchDesc(ctx: string, from: number) {
     // 在win下，其实不替换下面的html符号，也能正常显示，但linux下完全不行
 
     // 去掉换行(现在是按行解释，得到的换行并不是真的换行)
-    desc = desc.replace(/\r?\n/g, " ");
+    desc = desc.replace(/\r?\n/g, ' ');
 
     // g globa search，即搜索文中所有匹配的字符而不仅仅是第一次
     // m multi line，多行匹配
     // .+? 中的?表示非贪婪模式
     desc = desc.replace(/<code>(.+?)<\/code>/gm, (match, p1) => {
-        if (p1.startsWith("*")) {
+        if (p1.startsWith('*')) {
             return `__${p1}__`;
         }
         return `**${p1}**`;
     });
     desc = desc.replace(/<b>(.+?)<\/b>/gm, (match, p1) => {
         // 有些会同时被<a> <code>包括，已经替换的，不需要再替换
-        if (p1.startsWith("**") || p1.startsWith("__")) {
+        if (p1.startsWith('**') || p1.startsWith('__')) {
             return p1;
         }
         return `**${p1}**`;
@@ -176,36 +190,36 @@ function searchDesc(ctx: string, from: number) {
         return p1;
     });
     // 次方符号，其实有些markdown是能解析的，比如markdown，但vscode不行
-    // 
+    //
     desc = desc.replace(/<sup>(.+?)<\/sup>/gm, (match, p1) => {
         return `^${p1}`;
     });
 
     desc = desc.replace(/<a .+>(.+?)<\/a>/gm, (match, p1) => {
         // 有些会同时被<a> <code>包括，已经替换的，不需要再替换
-        if (p1.startsWith("**") || p1.startsWith("__")) {
+        if (p1.startsWith('**') || p1.startsWith('__')) {
             return p1;
         }
         return `**${p1}**`;
     });
-    desc = desc.replace("<p>", "");
+    desc = desc.replace('<p>', '');
     // html的横杠替换成真正的-
-    desc = desc.replace(/&ndash;/g, "-");
+    desc = desc.replace(/&ndash;/g, '-');
     // html的&替换成真正的&
-    desc = desc.replace(/&sect;/g, "&");
+    desc = desc.replace(/&sect;/g, '&');
     // html的不换行空格替换成真正的空格
-    desc = desc.replace(/&nbsp;/g, " ");
+    desc = desc.replace(/&nbsp;/g, ' ');
     // html的小于号替换成真正的<
-    desc = desc.replace(/&lt;/g, "<");
+    desc = desc.replace(/&lt;/g, '<');
     // 把html中的...替换成真正的...
-    desc = desc.replace(/&middot;&middot;&middot;/g, "...");
+    desc = desc.replace(/&middot;&middot;&middot;/g, '...');
 
     // 替换<pre> ... </pre> 这部分内容里一般都是代码
     // desc = desc.replace(/\<pre\>(.+?)\<\/pre\>/gm, (match, p1) => {
     //     return `${p1}`;
     // });
-    desc = desc.replace("<pre>", "\n\n");
-    desc = desc.replace("</pre>", "\n\n");
+    desc = desc.replace('<pre>', '\n\n');
+    desc = desc.replace('</pre>', '\n\n');
 
     return desc;
 }
@@ -217,7 +231,7 @@ function searchDesc(ctx: string, from: number) {
  */
 function searchDecl(ctx: string, name: string): SymbolEx | null {
     const begStr = `<hr><h3><a name="pdf-${name}"><code>`;
-    const endStr = "</code></a></h3>\n";
+    const endStr = '</code></a></h3>\n';
 
     const basePos = ctx.indexOf(begStr);
     if (basePos < 0) {
@@ -236,15 +250,15 @@ function searchDecl(ctx: string, name: string): SymbolEx | null {
         assert(name === matchs[1]);
 
         // 把html中的...替换成真正的...
-        const rawParam = matchs[2].replace(/&middot;&middot;&middot;/g, "...");
+        const rawParam = matchs[2].replace(/&middot;&middot;&middot;/g, '...');
 
         // 替换掉可选参数，不然singalhelp那里无法分解参数
-        let paramStr = rawParam.replace(/ \[,/g, ",");
-        paramStr = paramStr.replace(/]/g, "");
-        paramStr = paramStr.replace(/\[/g, "");
+        let paramStr = rawParam.replace(/ \[,/g, ',');
+        paramStr = paramStr.replace(/]/g, '');
+        paramStr = paramStr.replace(/\[/g, '');
 
-        parameters = paramStr.split(", ");
-    } else if ("_G" === name) {
+        parameters = paramStr.split(', ');
+    } else if ('_G' === name) {
         kind = SymbolKind.Namespace;
     } else {
         // _VERSION、math.pi、math.huge is variable
@@ -252,7 +266,7 @@ function searchDecl(ctx: string, name: string): SymbolEx | null {
     }
 
     let base;
-    const baseEndPos = name.indexOf(".");
+    const baseEndPos = name.indexOf('.');
     if (baseEndPos > 0) {
         base = name.substring(0, baseEndPos);
         name = name.substring(baseEndPos + 1);
@@ -261,17 +275,20 @@ function searchDecl(ctx: string, name: string): SymbolEx | null {
     const desc = searchDesc(ctx, endPos + endStr.length);
 
     return {
-        url: "",
+        url: '',
         name: name,
         base: base,
         kind: kind,
         comment: desc,
-        parameters: parameters
+        parameters: parameters,
     };
 }
 
 function searchNamespace(
-    ctx: string, url: string, name: string): SymbolEx | null {
+    ctx: string,
+    url: string,
+    name: string,
+): SymbolEx | null {
     //lua5.3 <h2>6.8 &ndash; <a name="6.8">
     let begStr = `<h2>${url} &ndash; <a name="${url}">`;
 
@@ -286,14 +303,14 @@ function searchNamespace(
         }
     }
 
-    const endPos = ctx.indexOf("\n", basePos) + 1;
+    const endPos = ctx.indexOf('\n', basePos) + 1;
     const desc = searchDesc(ctx, endPos);
 
     return {
-        url: "",
+        url: '',
         name: name,
         kind: SymbolKind.Namespace,
-        comment: desc
+        comment: desc,
     };
 }
 
@@ -317,7 +334,7 @@ function search(ctx: string, sym: SymbolEx): SymbolEx | null {
 function main() {
     console.log(`Current directory: ${process.cwd()}`);
 
-    stl.forEach(v => {
+    stl.forEach((v) => {
         console.log(`start search symbol for lua ${v.v}`);
 
         const cctx = fs.readFileSync(v.c).toString();
@@ -329,10 +346,10 @@ function main() {
 
         // lua5.1的contents.html中不包含coroutine的模块声明，需要手动加上
 
-        assert(symbols.length > 0, "no symbol found ...");
+        assert(symbols.length > 0, 'no symbol found ...');
         const finalSymbols: SymbolEx[] = [];
-        symbols.forEach(s => {
-            if (s.name === "coroutine") {
+        symbols.forEach((s) => {
+            if (s.name === 'coroutine') {
                 console.log(JSON.stringify(s));
             }
             const sym = search(mctx, s);
